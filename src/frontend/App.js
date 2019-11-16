1a7e2a08888a28e1c6d8d8613d096dc1282dd690
unknown <nicolascc1@hotmail.com>
2019-11-16 20:53:07 +0000

    Task #1 Fav movie covers

---
+++ src/frontend/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../frontend/App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Welcome to Kodflix</h1>
+    </div>
+  );
+}
+
+export default App;
