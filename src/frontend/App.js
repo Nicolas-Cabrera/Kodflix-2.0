fd77888dcc70f1079cbf251bf8ad67ff4e5bb883
unknown <nicolascc1@hotmail.com>
2019-11-16 21:32:03 +0000

    Task #1 done

--- src/frontend/App.js
+++ src/frontend/App.js
@@ -1,10 +1,26 @@
 import React from 'react';
 import '../frontend/App.css';
+import BreakingB from './Images/breakingbad.jpg';
+import CasaDePapel from './Images/casadepapel.jpg';
+import HarryPotter from './Images/harrypotter.jpg';
+import Insidious from './Images/insidious.jpg';
+import Flash from './Images/theflash.jpg';
+import Xmen from './Images/xmen.jpg';
 
 function App() {
   return (
     <div className="App">
       <h1>Welcome to Kodflix</h1>
+      <div>
+        <div className='item'>Breaking Bad</div>
+        <div className='item'>Casa de Papel</div>
+        <div className='item'>Harry Potter</div>
+      </div>
+      <div>
+      <div className='item'>Insidious</div>
+      <div className='item'>The Flash</div>
+      <div className='item'>X-men</div>
+      </div>
     </div>
   );
 }
