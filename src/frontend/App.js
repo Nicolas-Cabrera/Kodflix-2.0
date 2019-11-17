eb544fa63ac42547d1e27172e85e8fee58fdae1c
unknown <nicolascc1@hotmail.com>
2019-11-17 20:05:34 +0000

    Task #5 done

--- src/frontend/App.js
+++ src/frontend/App.js
@@ -14,23 +14,41 @@ function App() {
       <div className='container'>
         <div className='item'>
           <img src={BreakingB} alt='breakingbad' />
+          <div className='overlay'>
+            <h1>Breaking Bad</h1>
+          </div>
         </div>
         <div className='item'>
           <img src={CasaDePapel} alt='casadepapel' />
+          <div className='overlay'>
+            <h1>Casa De Papel</h1>
+          </div>
         </div>
         <div className='item'>
           <img src={HarryPotter} alt='harrypotter' />
+          <div className='overlay'>
+            <h1>Harry Potter</h1>
+          </div>
         </div>
       </div>
       <div className='container'>
         <div className='item'>
           <img src={Insidious} alt='insidious' />
+          <div className='overlay'>
+            <h1>Insidious</h1>
+          </div>
         </div>
         <div className='item'>
           <img src={Flash} alt='flash' />
+          <div className='overlay'>
+            <h1>The Flash</h1>
+          </div>
         </div>
-        <div className='item'>
+        <div className='item'> 
           <img src={Xmen} alt='xmen' />
+          <div className='overlay'>
+            <h1>X-Men</h1>
+          </div>
         </div>
       </div>
     </div>
