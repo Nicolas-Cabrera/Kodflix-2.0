1d1ecd614523c2dc5be1461783bf7b319549bd42
unknown <nicolascc1@hotmail.com>
2019-11-20 16:05:51 +0000

    Task #14 done - Use params to show movie title on detail page

--- src/frontend/Details/Details.js
+++ src/frontend/Details/Details.js
@@ -1,42 +1,23 @@
-import React, { Component, useState, useEffect } from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState, useEffect} from 'react';
+import { useParams } from 'react-router-dom';
+import getMovies from '../getMovies';
 
-export default function Details() {
-    let [message, setMessage] = useState('Hello, this will be the details page for each Movie & TV show :)');
+export default function Details(props) {
+
+    const { movieId } = useParams();
+    let [movie, setMovie] = useState('');
+
     useEffect(() => {
-        setTimeout(() => {
-            setMessage('Coming Soon! :)');
-        }, 3000);
-    });
-
+        let movie = getMovies().find((movie) => {
+        return movie.id === movieId;
+        });
+        setMovie(movie);
+    }, [movieId]);
+    
     return (
         <div>
             <h1>Welcome to details page</h1>
-            <h2>{message}</h2>
+            <h1>{movie.name}</h1>
         </div>
     );
 }
-
-//Class way of doing it
-// export default class Details extends Component {
-//     constructor(){
-//         super();
-//         this.state = {
-//             welcomeMessage: 'Hello, this will be the details page for each Movie & TV show :)'
-//         };
-//     }
-//     componentDidMount() {
-//         setTimeout(() => {
-//             this.setState({welcomeMessage: 'Coming Soon! :)'});
-//         }, 3000);
-//     }
-//     render() {
-//         return (
-//             <>
-//                 <h1>Welcome to details page</h1>
-//                 <h2>{this.state.welcomeMessage}</h2>
-//                 <Link to='/'>Back to home page</Link>
-//             </>
-//         )
-//     }
-// }
