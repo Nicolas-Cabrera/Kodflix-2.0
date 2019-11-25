31bff41fa94caa8f39b724faf2177ec627c36d13
unknown <nicolascc1@hotmail.com>
2019-11-25 12:44:40 +0000

    Task #19 list of movies from backend

--- src/backend/app.js
+++ src/backend/app.js
@@ -1,6 +1,13 @@
 const express = require('express');
 const app = express();
 const port = 3001;
+const getMovies = require('./movies');
 
-app.get('/', (req, res) => res.send('Hello Gideon'));
+app.get('/', (req, res) => res.send('Hello backend'));
 app.listen(port, () => console.log(`Example ${port}`));
+
+//api for movies
+app.get('/rest/movies', (rep, res) => {
+    //let movies = getMovies();
+    res.send(getMovies());
+});
