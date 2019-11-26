bc399e7d9126b98b453d672cba886ea7608d2208
unknown <nicolascc1@hotmail.com>
2019-11-26 15:53:43 +0000

    Task #22 front and backend connected

--- src/backend/app.js
+++ src/backend/app.js
@@ -7,7 +7,12 @@ app.get('/', (req, res) => res.send('Hello backend welcome back Nicolas'));
 app.listen(port, () => console.log(`Example ${port}`));
 
 //api for movies
-app.get('/rest/movies', (rep, res) => {
+app.get('/rest/movies', (req, res) => {
     //let movies = getMovies();
     res.send(getMovies());
+});
+
+app.get('/rest/movies/:id', (req, res) => {
+    // let movie = getMovies();
+    res.send(getMovies(req.params.id));
 });
--- src/frontend/App.js
+++ src/frontend/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import '../frontend/App.css';
 import Details from '../frontend/Details/Details';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
@@ -6,8 +6,24 @@ import Gallery from './Gallery';
 import notFound from './NotFound';
 
 function App() {
+
+  let [movies, setMovies] = useState();
+
+  useEffect(() => {
+    fetch('/rest/movies')
+        .then(response => response.json())
+        .then(movies => setMovies(movies))
+  }, []);
+
   return (
     <Router>
+      {/* <h3>Titles</h3>
+      {
+        movies ? movies.map(movie => {
+           return <div>Name: {movie.name}</div>
+         }) :
+         <div>No data</div>
+      } */}
       <Switch>
         <div className="App">
           <Route exact path='/' component={Gallery} />
