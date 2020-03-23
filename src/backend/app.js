4f6ee32c9df8948e1d16d09ab0f47c285e58f925
Nicolas Cabrera <nicolascc1@hotmail.com>
2020-03-23 16:49:05 +0000

    Task 28 - app connected to the database

--- src/backend/app.js
+++ src/backend/app.js
@@ -2,20 +2,30 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const port = process.env.PORT || 3001;
-const getMovies = require('./movies');
+const db = require('./db');
+//const getMovies = require('./movies');
 
-//api for movies
-app.get('/rest/movies', (req, res) => {
-    //let movies = getMovies();
-    res.send(getMovies());
+db.connect().then(dbo => {
+	app.get('/rest/movies', (req, res) => {
+		dbo.collection('shows').find({}).toArray((err, results) => {
+			if(err) throw err;
+			res.send(results);
+		})
+	})
+
+
+// //api for movies
+// app.get('/rest/movies', (req, res) => {
+//     //let movies = getMovies();
+//     res.send(getMovies());
+// });
+
+	app.use(express.static(path.join(__dirname, '../../build')));
+
+	app.get('/', (req, res) => res.send('Hello backend welcome back sir'));
+	app.listen(port, () => console.log(`Example ${port}`));
+
+	app.get('*', function (req, res) {
+		res.sendFile(path.join(__dirname, '../../build', 'index.html'));
+	});
 });
-
-app.use(express.static(path.join(__dirname, '../../build')));
-
-app.get('/', (req, res) => res.send('Hello backend welcome back'));
-app.listen(port, () => console.log(`Example ${port}`));
-
-app.get('*', function (req, res) {
-    res.sendFile(path.join(__dirname, '../../build', 'index.html'));
-});
-
--- src/backend/db.js
+++ src/backend/db.js
@@ -1,13 +1,18 @@
 const MongoClient = require('mongodb').MongoClient;
 const assert = require('assert');
 
-const url = 'mongodb://localhost:27017';
-const dbName = 'kodflix';
+//const url = 'mongodb://Nico:Nico1216@localhost:27017/kodflix';
+const url = 'mongodb+srv://Nico:Nico1216@cluster0-hr8ag.mongodb.net/test?retryWrites=true&w=majority';
 
-MongoClient.connect(url, function(err, client) {
-	assert.equal(null, err);
-	console.log('Connected successfully to server');
+module.exports = { connect };
 
-	const db = client.db(dbName);
-	client.close();
-});
+function connect() {
+	return new Promise((resolve, reject) => {
+		MongoClient.connect(url, function (err, client) {
+			assert.equal(null, err);
+			console.log('Connected successfully to server');
+			const dbo = client.db('kodflix');
+			resolve(dbo);
+		});
+	})
+}
