e4b954deb9374fece48681ed9b0b1c14643e6cad
Nicolas Cabrera <nicolascc1@hotmail.com>
2020-03-24 15:12:46 +0000

    putting the build above get test#122003213

--- src/backend/app.js
+++ src/backend/app.js
@@ -5,14 +5,14 @@ const port = process.env.PORT || 3001;
 const db = require('./db');
 
 db.connect().then(dbo => {
+	app.use(express.static(path.join(__dirname, '../../build')));
+
 	app.get('/rest/movies', (req, res) => {
 		dbo.collection('shows').find({}).toArray((err, results) => {
 			if(err) throw err;
 			res.send(results);
 		})
 	})
-
-	app.use(express.static(path.join(__dirname, '../../build')));
 
 	app.get('/', (req, res) => res.send('Hello backend welcome back sir!'));
 	app.listen(port, () => console.log(`Example ${port}`));
